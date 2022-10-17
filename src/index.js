a3d84d1155d7143d4bd03a5bf439fd363fba65d3
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-17 15:34:46 -0400

    Build dynamic menu in javascript with webpacker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,151 @@
 import './style.css';
 import Hamburger from './hamburger.png';
 import RadBarrel from './radioactive.png';
+import MoltenSkins from './potato-peeled.jpg';
+import RadFries from './rad-fries.jpg';
+import BlueSoda from './blue-soda.jpg';
+
+const menu = [{
+    name: "Appetizers",
+    content: [{
+            name: "Molten Skins",
+            description: "Hot, savory, melty, crispy potato skins",
+            price: "$3.50",
+            img: MoltenSkins,
+            alt: "A picture of molten skins",
+            attribution: '<a href="https://www.freepik.com/free-vector/potato-peeled-3d-isolated-realistic-potato-vegetable-whole-half-peeled_2238454.htm#query=potato%20skins&position=0&from_view=search&track=sph">Image by vectorpouch</a> on Freepik'
+        },
+        {
+            name: "Rad Fries",
+            description: "French fries but they're a little bitey",
+            price: "$1.25",
+            img: RadFries,
+            alt: "A picture of rad fries",
+            attribution: '<a href="https://www.freepik.com/free-vector/angry-french-fries-cartoon-character-monochrome-fast-food-ad-menu-card-design_31299746.htm#query=radioactive%20fries&position=10&from_view=search&track=ais">Image by Rochak Shukla</a> on Freepik'
+        }
+    ]
+},
+{
+    name: "Drinks",
+    content: [{
+        name: "Blue Barrel",
+        description: "A slurp as good as anything else on this menu",
+        price: "$2.00",
+        img: BlueSoda,
+        alt: "A picture of soda",
+        attribution: '<a href="https://www.freepik.com/free-vector/realistic-soda-with-ice-advertisement_4398928.htm#query=soda&position=17&from_view=search&track=sph">Image by pikisuperstar</a> on Freepik'
+    }]
+},
+{
+    name: "Main Dishes",
+    content: [{
+            name: "Radburger",
+            description: "Our classic burger with moderate amounts of radiation",
+            price: "$1.50",
+            img: "",
+            alt: "A picture of radburger",
+            attribution: ''
+        },
+        {
+            name: "Radburger with Cheese",
+            description: "Our classic burger with cheese",
+            price: "$2.00",
+            img: "",
+            alt: "A picture of radburger with cheese",
+            attribution: ''
+        },
+        {
+            name: "Radburger Deluxe",
+            description: "Our classic burger with cheese AND bacon",
+            price: "$4.00",
+            img: "",
+            alt: "A picture of radburger with cheese AND backon",
+            attribution: ''
+        }
+    ]
+},
+{
+    name: "Desserts",
+    content: [{
+        name: "Susquehanna River Milkshake",
+        description: "Made with fresh milk from the surrounding river",
+        price: "$5.00",
+        img: "",
+        alt: "A picture of a milkshake",
+        attribution: ''
+    }]
+}
+];
+
+function buildMenu(menu) {
+    
+    const radContent = document.getElementById("radContent");
+    radContent.innerHTML = "";
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList = "radBlock title";
+
+    const myFirstRadBarrel = new Image();
+    myFirstRadBarrel.src = RadBarrel;
+    myFirstRadBarrel.setAttribute("width", "50px");
+
+    const mySecondRadBarrel = new Image();
+    mySecondRadBarrel.src = RadBarrel;
+    mySecondRadBarrel.setAttribute("width", "50px");
+
+    const titleText = document.createElement("h1");
+    titleText.textContent = "Harrisburgers Menu";
+
+    titleDiv.appendChild(myFirstRadBarrel);
+    titleDiv.appendChild(titleText);
+    titleDiv.appendChild(mySecondRadBarrel);
+
+    radContent.appendChild(titleDiv);
+
+    menu.forEach((section) => {
+
+        const newTitleBlock = document.createElement("div");
+        newTitleBlock.classList = "radBlock menuTitle";
+
+        const titleText = document.createElement("h2");
+        titleText.textContent = section.name;
+
+        newTitleBlock.appendChild(titleText);
+        radContent.appendChild(newTitleBlock);
+
+        section.content.forEach((item) => {
+
+            const itemDiv = document.createElement('div');
+            itemDiv.classList = "radBlock";
+
+            const itemTitle = document.createElement('h3');
+            itemTitle.classList = "itemTitle";
+            itemTitle.textContent = item.name;
+            itemDiv.appendChild(itemTitle);
+
+            const itemDescription = document.createElement('p');
+            itemDescription.classList = "description";
+            itemDescription.textContent = item.description;
+            itemDiv.appendChild(itemDescription);
+
+            const itemPrice = document.createElement('h4');
+            itemPrice.classList = "price";
+            itemPrice.textContent = item.price;
+            itemDiv.appendChild(itemPrice);
+
+            const itemImg = document.createElement('img');
+            itemImg.classList = "itemImage";
+            itemImg.src = item.img;
+            itemImg.width = "100";
+            itemImg.height = "100";
+            itemDiv.appendChild(itemImg);
+
+            radContent.appendChild(itemDiv);
+
+        })
+    })
+
+}
 
 function buildNavBar() {
     const navBar = document.querySelector('.navBar');
@@ -14,35 +159,43 @@ function buildNavBar() {
         myHamburger.src = Hamburger;
         myHamburger.setAttribute("width", "50px");
         myHamburger.textContent = "MENU";
-    
-        
+
+
         navItem.appendChild(myHamburger);
 
         const navText = document.createElement('div');
         navText.classList = "navText";
         navText.textContent = item;
-        
+
         navItem.appendChild(navText);
+        navItem.addEventListener('click', (e) => {
+            if (item === "Home") {
+                buildRadBlocks();
+            } else if (item === "Menu") {
+                buildMenu(menu);
+            } else if (item === "Contact") {
+                
+            }
+        });
         navBar.appendChild(navItem);
     }))
 
 }
 
 function buildRadBlocks() {
-    const radContent = document.getElementById("radContent");
+    const radContent = document.getElementById("radContent");    
+    radContent.innerHTML = "";
 
     const titleDiv = document.createElement("div");
     titleDiv.classList = "radBlock title";
 
-    const myFirstRadBarrel= new Image();
+    const myFirstRadBarrel = new Image();
     myFirstRadBarrel.src = RadBarrel;
     myFirstRadBarrel.setAttribute("width", "50px");
-    myFirstRadBarrel.textContent = "MENU";
-    
+
     const mySecondRadBarrel = new Image();
     mySecondRadBarrel.src = RadBarrel;
     mySecondRadBarrel.setAttribute("width", "50px");
-    mySecondRadBarrel.textContent = "MENU";
 
     const titleText = document.createElement("h1");
     titleText.textContent = "Harrisburgers";
@@ -92,6 +245,7 @@ function buildRadBlocks() {
     radContent.appendChild(hoursDiv);
     radContent.appendChild(addressDiv);
 }
+
 
 buildNavBar();
 buildRadBlocks();
